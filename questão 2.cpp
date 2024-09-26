ad4f9eefdee6efc15338fc8ad7b87bf381942048
Sarah Cristina Freitas da Silva <163673649+SarahCristina00@users.noreply.github.com>
2024-09-26 15:15:44 -0300

    Update questão 2.cpp

--- questão 2.cpp
+++ questão 2.cpp
@@ -1,9 +1,4 @@
-/******************************************************************************
 
-Escreva um programa que verifique, em uma string, a existência da letra ‘a’, 
-seja maiúscula ou minúscula, além de informar a quantidade de vezes em que ela
-ocorre.
-*******************************************************************************/
 #include <iostream>
 #include<cstring>
 
